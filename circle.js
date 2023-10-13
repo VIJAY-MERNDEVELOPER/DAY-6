096ee1455e29d9622b0f45a8dab1a0e0a5358fed
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-10-13 22:27:55 +0530

    commit

--- circle.js
+++ circle.js
@@ -18,14 +18,43 @@ class Circle {
         color  : ${this._color}
                 `;
   }
+
+  get circleRadius() {
+    return `Radius: ${this._radius}`;
+  }
+
+  set newRadius(modifiedRadius) {
+    console.log((this._radius = modifiedRadius));
+  }
+
+  setcolor(modifiedcolor) {
+    console.log(this._color=modifiedcolor);
+  }
+
+  toString() {
+    return `
+    Radius in string: ${+this._radius},
+    Color in string: ${this._color}
+    `;
+  }
 }
 
 const circleObj = new Circle(20, "blue");
 console.log("Area of Circle          :", circleObj.getArea().toFixed(2));
 
-console.log(  "Circumeference of Circle:", circleObj.getCircumeference().toFixed(2));
+console.log(
+  "Circumeference of Circle:",
+  circleObj.getCircumeference().toFixed(2)
+);
 
 console.log(circleObj.getValues());
 
+console.log(circleObj.circleRadius);
 
+circleObj.newRadius = 4;
+console.log("New Area of Circle:", circleObj.getArea().toFixed(2));
 
+console.log(circleObj.toString());
+
+circleObj.setcolor("Brown");
+console.log("New color of circle:", circleObj.getValues());
