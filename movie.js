c481517117a51e8ba455d1e6be2412187768d548
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-10-14 08:09:46 +0530

    commit

--- movie.js
+++ movie.js
@@ -13,9 +13,14 @@ class movie {
     `;
   }
 
-  //   getPG(movie){
-  //     for()
-  //   }
+  getPG(movies) {
+    for (const movie of movies) {
+      if (movie._rating === "PG") {
+        pgMovies.push(movie);
+      }
+    }
+    return pgMovies;
+  }
 }
 
 /*a) Write a constructor for the class Movie, which takes a String representing the title of the movie,
@@ -31,13 +36,19 @@ when no rating is provided. */
 const movieObj2 = new movie("Shot Boot 3", "Universe Creations");
 console.log(movieObj2.movieDetails());
 
-/*c) Write a method getPG, which takes an array of base type Movie as its argument,
-and returns a new array of only those movies in the input array with a rating of "PG".
-You may assume the input array is full of Movie instances.
-The returned array need not be full.*/
-
 /*d) Write a piece of code that creates an instance of the class Movie with the
 title “Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”  */
 
 const movieObj3 = new movie("casino Royale", "Eon Productions", "PG13");
 console.log(movieObj3.movieDetails());
+
+/*c) Write a method getPG, which takes an array of base type Movie as its argument,
+and returns a new array of only those movies in the input array with a rating of "PG".
+You may assume the input array is full of Movie instances.
+The returned array need not be full.*/
+
+const movieObj4 = new movie("Home Alone", "Hughes Entertainment", "PG");
+
+const movies = ["Jailer", "Shot Boot 3", "casino Royale", "Home Alone"];
+
+console.log(movie.getPG);
